a5de1dcd58cccb61d020e5ecc2c98bc001e3ab19
Leonard Hansing <lhansing92@gmail.com>
2025-10-31 16:49:55 +0000

    temp

--- src/quaternion.rs
+++ src/quaternion.rs
@@ -35,6 +35,48 @@ impl Guest for Component {
     }
 
     fn to_matrix3x3(q: Quaternion) -> Matrix3x3 {
-        todo!()
+        let x = q.0;
+        let y = q.1;
+        let z = q.2;
+        let w = q.3;
+        let x2 = x + x;
+        let y2 = y + y;
+        let z2 = z + z;
+        let xx = x * x2;
+        let xy = x * y2;
+        let xz = x * z2;
+        let yy = y * y2;
+        let yz = y * z2;
+        let zz = z * z2;
+        let wx = w * x2;
+        let wy = w * y2;
+        let wz = w * z2;
+
+        te[0] = (1 - (yy + zz));
+        te[1] = (xy + wz);
+        te[2] = (xz - wy);
+        te[3] = 0;
+
+        te[4] = (xy - wz);
+        te[5] = (1 - (xx + zz));
+        te[6] = (yz + wx);
+        te[7] = 0;
+
+        te[8] = (xz + wy);
+        te[9] = (yz - wx);
+        te[10] = (1 - (xx + yy));
+        te[11] = 0;
+
+        Matrix3x3 {
+            m00: (),
+            m10: (),
+            m20: (),
+            m01: (),
+            m11: (),
+            m21: (),
+            m02: (),
+            m12: (),
+            m22: (),
+        }
     }
 }
